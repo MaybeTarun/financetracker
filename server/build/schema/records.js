ce987f31e8efbc2992fa232ff9c7bc184d17fb44
Tarun Gupta <tarunthegreat2003@gmail.com>
2024-07-15 17:13:41 +0530

    vercel

---
+++ server/build/schema/records.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const recordsSchema = new mongoose_1.default.Schema({
+    userId: { type: String, required: true },
+    date: { type: Date, required: true },
+    description: { type: String, required: true },
+    amount: { type: Number, required: true },
+    category: { type: String, required: true },
+    payment: { type: String, required: true },
+});
+const RecordsModel = mongoose_1.default.model("Records", recordsSchema);
+exports.default = RecordsModel;
